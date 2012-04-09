1d5a760f31fb04efae482ad581c37ec54349fbea
Athammai Thiagarajan <athiagarajan@reisystems.com>
2012-04-09 17:32:02 +0000

    Changes to fact_revenue
    git-svn-id: https://omb-dashboardplatform.reisys.com:10039/NYC_Checkbook_2_0/Database@646 948744eb-f611-4f28-8820-250fb6d8f47d
    
    Former-commit-id: 066d6a03adbd9e2dcaba320f0252793dd7c2ed9a

--- checkbook/DB scripts/ETLScripts/RevenueScripts.sql
+++ checkbook/DB scripts/ETLScripts/RevenueScripts.sql
@@ -781,16 +781,24 @@ BEGIN
 	INSERT into fact_revenue(revenue_id,fiscal_year,fiscal_period,posting_amount,
 				revenue_category_id,revenue_source_id,fiscal_year_id,agency_id,
 				department_id,revenue_class_id,fund_class_id,funding_class_id,
-				budget_code_id,budget_year_id)
+				budget_code_id,budget_fiscal_year_id,agency_name,revenue_category_name,
+				revenue_source_name,budget_fiscal_year,department_name,revenue_class_name,
+				fund_class_name,funding_class_name)
 	SELECT  a.revenue_id,a.fiscal_year,a.fiscal_period,a.posting_amount,
 		a.revenue_category_id,a.revenue_source_id,d.year_id,b.agency_id,
 		c.department_id,a.revenue_class_id,a.fund_class_id,e.funding_class_id,
-		a.budget_code_id,f.year_id
+		a.budget_code_id,f.year_id,b.agency_name,g.revenue_category_name,
+		e.revenue_source_name,a.budget_fiscal_year,c.department_name,i.revenue_class_name,
+		j.fund_class_name,k.funding_class_name
 	FROM    revenue a join ref_agency_history b on a.agency_history_id = b.agency_history_id
 		join ref_department_history c on a.department_history_id = c.department_history_id
 		join ref_year d on a.fiscal_year = d.year_value
 		join ref_revenue_source e on a.revenue_source_id = e.revenue_source_id
-		join ref_year f on a.budget_fiscal_year = f.year_value;
+		join ref_year f on a.budget_fiscal_year = f.year_value
+		join ref_revenue_category g on a.revenue_category_id = g.revenue_category_id
+		join ref_revenue_class i on a.revenue_class_id = i.revenue_class_id
+		join ref_fund_class j on a.fund_class_id = j.fund_class_id
+		join ref_funding_class k on e.funding_class_id = k.funding_class_id;
 	*********************/
 	RETURN 1;
 	
