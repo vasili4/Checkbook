8cd7052e033234206d514e3f464287941118c9d6
Athammai Thiagarajan <athiagarajan@reisystems.com>
2012-04-20 15:17:57 +0000

    Kept the absolute path for the directory name. Also file name is now passed as a parameter to the shell script
    git-svn-id: https://omb-dashboardplatform.reisys.com:10039/NYC_Checkbook_2_0/Database@739 948744eb-f611-4f28-8820-250fb6d8f47d
    
    Former-commit-id: 43c2cfd8e26932dd1c9f5af0ff1abadccaa0cd49

--- checkbook/PreProcessingScripts/master_preprocess.sh
+++ checkbook/PreProcessingScripts/master_preprocess.sh
@@ -3,59 +3,48 @@ echo $1
 
 rm -f goodfile.txt
 rm -f badfile.txt
-dirName="datafiles/"
-if [ $1 == "COAAgency" ] ; then
-	fileName=$dirName"COA_agency_feed.txt"
-	./removeMalformedAgencyRecords.sh $fileName
+dirName="/home/gpadmin/athiagarajan/NYC/datafiles/"
+fileName=$dirName$2
+if [ $1 == "COAAgency" ] ; then	
+	./removeMalformedAgencyRecords.sh 
 	
 elif [ $1 == "COABudgetCode" ] ; then
-	./removeMalformedBudgetCodeRecords.sh datafiles/COA_budget_code_feed.txt
+	./removeMalformedBudgetCodeRecords.sh $fileName
+	
 elif [ $1 == "COADepartment" ] ; then
-	fileName=$dirName"COA_department_feed.txt"
 	./removeMalformedDepartmentRecords.sh $fileName
 	
 elif [ $1 == "COAExpenditureObject" ] ; then
-	fileName=$dirName"COA_expenditure_object_feed.txt"
 	./removeMalformedExpenditureObjectRecords.sh $fileName
 	 
 elif [ $1 == "COALocation" ] ; then
-	fileName=$dirName"COA_location_object_feed.txt"
 	./removeMalformedLocationRecords.sh $fileName
 	 
 elif [ $1 == "COAObjectClass" ] ; then
-	fileName=$dirName"COA_object_class_feed.txt"
 	./removeMalformedObjectClassRecords.sh $fileName
 	 
 elif [ $1 == "COARevenueCategory" ] ; then
-	fileName=$dirName"COA_revenue_category_feed.txt"
 	./removeMalformedRevenueCategoryRecords.sh $fileName
 	 
 elif [ $1 == "COARevenueClass" ] ; then
-	fileName=$dirName"COA_revenue_class_feed.txt"
 	./removeMalformedRevenueClassRecords.sh $fileName
 	 
 elif [ $1 == "COARevenueSource" ] ; then
-	fileName=$dirName"COA_revenue_source_feed.txt"
 	./removeMalformedRevenueSourceRecords.sh $fileName
 	 
 elif [ $1 == "CON" ] ; then
-	fileName=$dirName"CON_feed.txt"
 	./removeMalformedCONRecords.sh $fileName
 	 
 elif [ $1 == "FMS" ] ; then
-	fileName=$dirName"FMS_feed.txt"
 	./removeMalformedFMSRecords.sh $fileName
 	 
 elif [ $1 == "MAG" ] ; then
-	fileName=$dirName"MAG_feed.txt"
 	./removeMalformedMAGRecords.sh $fileName
 	 
 elif [ $1 == "Revenue" ] ; then
-	fileName=$dirName"Revenue_feed.txt"
 	./removeMalformedRevenueRecords.sh $fileName
 	 
 elif [ $1 == "Budget" ] ; then
-	fileName=$dirName"BUDGET_feed.txt"
 	./removeMalformedBudgetRecords.sh $fileName
 	
 fi
