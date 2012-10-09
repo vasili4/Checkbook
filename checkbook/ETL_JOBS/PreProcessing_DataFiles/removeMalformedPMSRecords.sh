5e2553c25988114a7d3d8c720854a955ec316e88
Tirupati Reddy <treddy@reisystems.com>
2012-10-09 15:26:30 +0000

    NYCCHKBK-1611
    git-svn-id: https://omb-dashboardplatform.reisys.com:10039/NYC_Checkbook_2_0/Database@3501 948744eb-f611-4f28-8820-250fb6d8f47d
    
    Former-commit-id: b66c1e7fffc99a55c38064a77d258e681eaf7eb9

--- checkbook/ETL_JOBS/PreProcessing_DataFiles/removeMalformedPMSRecords.sh
+++ checkbook/ETL_JOBS/PreProcessing_DataFiles/removeMalformedPMSRecords.sh
@@ -4,7 +4,7 @@
         line = $0;
         delimiter="|";
         no_of_attributes = split(line,attributes,delimiter);
-        if(no_of_attributes==20){
+        if(no_of_attributes==21){
                 print $0 >> "goodfile.txt"
         } else
         {
