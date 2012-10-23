ed622a4d5f3eb0a579601536ec33dd251e5770d5
Tirupati Reddy <treddy@reisystems.com>
2012-10-23 21:38:22 +0000

    NYCCHKBK-1203
    git-svn-id: https://omb-dashboardplatform.reisys.com:10039/NYC_Checkbook_2_0/Database@3612 948744eb-f611-4f28-8820-250fb6d8f47d
    
    Former-commit-id: 24f575dccc0262e11eab7f9afe26a76f164a835d

--- checkbook/ETL_JOBS/PreProcessing_DataFiles/removeMalformedPendingContracts.sh
+++ checkbook/ETL_JOBS/PreProcessing_DataFiles/removeMalformedPendingContracts.sh
@@ -4,7 +4,7 @@
         line = $0;
         delimiter="|";
         no_of_attributes = split(line,attributes,delimiter);
-        if(no_of_attributes==29){
+        if(no_of_attributes==31){
                 print $0 >> "goodfile.txt"
         } else
         {
