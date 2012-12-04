2cfde994205e0328ad908be051a5d144b62ec11a
Pratap Varma <pvarma@reisystems.com>
2012-12-04 23:07:24 +0000

    NYCCHKBK-2001 wrong ticket number
    git-svn-id: https://omb-dashboardplatform.reisys.com:10039/NYC_Checkbook_2_0/Database@4644 948744eb-f611-4f28-8820-250fb6d8f47d
    
    Former-commit-id: 6019b2f46112d4e6584e598d4533bec85fcd9931

--- checkbook/Data Model/shard_public.sql
+++ checkbook/Data Model/shard_public.sql
@@ -332,15 +332,16 @@ CREATE TABLE disbursement_line_item_details(
 	calendar_fiscal_year_id smallint,
 	calendar_fiscal_year smallint,
 	agreement_accounting_line_number integer,
-    agreement_commodity_line_number integer,
-    agreement_vendor_line_number integer, 
-    reference_document_number character varying,   
-    reference_document_code varchar(8),
+        agreement_commodity_line_number integer,
+        agreement_vendor_line_number integer, 
+        reference_document_number character varying,   
+        reference_document_code varchar(8),
 	contract_document_code varchar(8),
 	master_contract_document_code varchar(8),
 	file_type char(1),
 	load_id integer,
-    last_modified_date timestamp without time zone
+        last_modified_date timestamp without time zone,
+        job_id bigint
 	)
 DISTRIBUTED BY (disbursement_line_item_id);
 
@@ -1851,7 +1852,8 @@ CREATE TABLE agreement_snapshot(
 	  original_version_flag character(1),
   	  latest_flag character(1),
   	  load_id integer,
-      last_modified_date timestamp without time zone
+          last_modified_date timestamp without time zone,
+          job_id bigint
 ) DISTRIBUTED BY (original_agreement_id);
 
 CREATE TABLE agreement_snapshot_cy (LIKE agreement_snapshot) DISTRIBUTED BY (original_agreement_id);
