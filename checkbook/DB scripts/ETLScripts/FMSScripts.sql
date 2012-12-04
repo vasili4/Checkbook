cc19d6b7b6e63b2514b3378baac1f245d27ffd74
Pratap Varma <pvarma@reisystems.com>
2012-12-04 22:27:43 +0000

    NYCCHKBK-2001 wrong ticket number
    git-svn-id: https://omb-dashboardplatform.reisys.com:10039/NYC_Checkbook_2_0/Database@4639 948744eb-f611-4f28-8820-250fb6d8f47d
    
    Former-commit-id: 1dd81aa67987c9af965467d14a993c4c29ce3fbc

--- checkbook/DB scripts/ETLScripts/FMSScripts.sql
+++ checkbook/DB scripts/ETLScripts/FMSScripts.sql
@@ -1257,7 +1257,7 @@ BEGIN
 						budget_code_id,budget_code,budget_name,reporting_code,location_id,location_code,fund_class_name,fund_class_code,
 						spending_category_id,spending_category_name,calendar_fiscal_year_id,calendar_fiscal_year,
 						agreement_accounting_line_number, agreement_commodity_line_number, agreement_vendor_line_number, reference_document_number,reference_document_code,
-						load_id,last_modified_date,file_type)
+						load_id,last_modified_date,file_type,job_id)
 	SELECT  b.disbursement_line_item_id,a.disbursement_id,b.line_number,a.check_eft_issued_date_id,
 		f.nyc_year_id,l.year_value,f.calendar_month_id,
 		b.agreement_id,NULL as master_agreement_id,b.fund_class_id,
@@ -1278,7 +1278,7 @@ BEGIN
 		 END) as spending_category_name,x.year_id,x.year_value,
 		 b.agreement_accounting_line_number, b.agreement_commodity_line_number, b.agreement_vendor_line_number, b.reference_document_number,b.reference_document_code,
 		 coalesce(a.updated_load_id, a.created_load_id),
-		 coalesce(a.updated_date, a.created_date),b.file_type
+		 coalesce(a.updated_date, a.created_date),b.file_type,z.job_id
 		FROM disbursement a JOIN disbursement_line_item b ON a.disbursement_id = b.disbursement_id
 			JOIN ref_agency_history c ON b.agency_history_id = c.agency_history_id
 			JOIN ref_agency m on c.agency_id = m.agency_id
