e620218a2f2ad2cfc9d2653fa8843971c5d213b4
Pratap Varma <pvarma@reisystems.com>
2012-12-11 23:46:40 +0000

    NYCCHKBK-2001 wrong ticket number contracts spending transaction disbursement number
    git-svn-id: https://omb-dashboardplatform.reisys.com:10039/NYC_Checkbook_2_0/Database@4865 948744eb-f611-4f28-8820-250fb6d8f47d
    
    Former-commit-id: 3b803a0fdf4da966b3f16b27e72468a51fa2b50f

--- checkbook/Data Model/shard_public.sql
+++ checkbook/Data Model/shard_public.sql
@@ -1788,6 +1788,7 @@ CREATE TABLE contracts_spending_transactions(
 	disb_master_agreement_id  bigint,
 	disb_fiscal_year_id  smallint,
 	disb_check_eft_issued_cal_month_id integer,
+	disb_disbursement_number character varying(40),
 	status_flag char(1),
 	type_of_year char(1)
 ) DISTRIBUTED BY (disbursement_line_item_id)
