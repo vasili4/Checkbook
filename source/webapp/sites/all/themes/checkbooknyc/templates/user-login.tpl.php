efa235852aca4e76fe301be4590f26d1618fea97
Alicia Torkelson <atorkelson@reisystems.com>
2016-12-08 20:24:25 -0500

    Overriding template for admin login page to be themed with custom css

---
+++ source/webapp/sites/all/themes/checkbooknyc/templates/user-login.tpl.php
@@ -0,0 +1,7 @@
+
+<div style="background-color: #ffffff;width: 1024px;margin: 3.5em auto auto;height: 100%;padding-bottom: 400px;padding-top: 20px;">
+    <div class="checkbooknyc-user-login-form-wrapper" style="width: 500px; margin: 3.5em auto auto; border-style: solid;border-width: thin; border-radius: 20px; padding: 60px; border-color: #CCCCCC; margin-left: 200px;">
+        <div><?php print render($intro_text); ?></div>
+        <?php print drupal_render_children($form) ?>
+    </div>
+</div>
